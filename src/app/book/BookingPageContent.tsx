72102803688c98ae0186a633443f2a72e066af00
jgrodriguez9 <jgrodriguez.pro@gmail.com>
2025-09-13 17:44:10 -0500

    avios error moving to component due to the useSearchparam

---
+++ src/app/book/BookingPageContent.tsx
@@ -0,0 +1,204 @@
+"use client";
+
+import InputController from "@/components/controllers/InputController";
+import PhoneController from "@/components/controllers/PhoneController";
+import BannerReminder from "@/components/transfers/BannerReminder";
+import ResumeDescription from "@/components/transfers/ResumeDescription";
+import ResumePayment from "@/components/transfers/ResumePayment";
+import { Button } from "@/components/ui/button";
+import { EMAIL_INVALID, FIELD_REQUIRED } from "@/constant/messages";
+import { useDecodedSearchParams } from "@/hooks/useDecodedSearchParams";
+import useSonner from "@/hooks/useSonner";
+import { parseDate } from "@/libs/dates";
+import { createTransferReservation } from "@/services/reservation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import z from "zod";
+
+const bookSchema = z
+  .object({
+    name: z.string().trim().min(1, FIELD_REQUIRED),
+    phone: z.string().trim().min(1, FIELD_REQUIRED),
+    email: z.email(EMAIL_INVALID),
+  })
+  .strict();
+
+export type BookData = z.infer<typeof bookSchema>;
+
+const BookingPageContent: React.FC = () => {
+  const sonner = useSonner();
+
+  const searchParams = useDecodedSearchParams();
+  const router = useRouter();
+  const { mutate, isPending } = useMutation({
+    mutationKey: ["createTransferReservation"],
+    mutationFn: createTransferReservation,
+    onSuccess: () => {
+      // Maneja el éxito de la mutación, como redirigir o mostrar un mensaje
+      router.push("/done");
+    },
+    onError: (error) => {
+      // Maneja el error de la mutación, como mostrar un mensaje de error
+      console.error(error);
+      sonner.simpleError(error);
+    },
+  });
+
+  const {
+    control,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<BookData>({
+    resolver: zodResolver(bookSchema),
+    defaultValues: {
+      name: "",
+      phone: "",
+      email: "",
+    },
+  });
+
+  const onHandleClickBook = handleSubmit((data) => {
+    //console.log(data);
+    //console.log(searchParams);
+    const { paxes } = searchParams.booking;
+    const { price } = searchParams.booking.vehicle;
+    const reservationData = {
+      originZoneId: searchParams.booking.originZoneId?._id,
+      destinationZoneId: searchParams.booking.destinationZoneId?._id,
+      vehicle: searchParams.booking.vehicle._id,
+      date: searchParams.booking.date,
+      hour: searchParams.booking.hour,
+      price: searchParams.booking.vehicle.isShared
+        ? price * (paxes.adults + (paxes.children || 0))
+        : price,
+      paxes: paxes,
+      currency: searchParams.booking.vehicle.currency,
+      name: data.name,
+      email: data.email,
+      phone: data.phone,
+    };
+    console.log(reservationData);
+    // Envía la petición de reservación solo si el formulario es válido
+    mutate(reservationData);
+    // Después se puede redirigir a done en onSuccess
+  });
+
+  return (
+    <div className="relative h-screen">
+      <div className="mt-4 p-4 lg:px-0 lg:mt-8 flex flex-col gap-14 justify-center max-w-5xl mx-auto">
+        <h1 className="text-2xl font-semibold">
+          Asegura tu reservación. ¡Solo te toma 2 minutos!
+        </h1>
+        <div className="grid grid-cols-1 lg:grid-cols-7 gap-7">
+          <div className="col-span-1 lg:col-span-4">
+            <div className="flex flex-col gap-3">
+              <div className="rounded-lg border border-gray-200 bg-background p-3  md:flex md:gap-6">
+                <BannerReminder dateReservation={searchParams.booking.date} />
+              </div>
+              <div className="rounded-lg border border-gray-200 bg-background p-3 py-6 md:gap-6">
+                <div className="flex flex-col gap-4">
+                  <h2 className="text-lg font-semibold">¿Quién viaja?</h2>
+                  <InputController
+                    label="Nombre completo"
+                    type="text"
+                    className="w-full lg:w-2/3"
+                    placeholder="Ingresa tu nombre"
+                    error={errors.name?.message}
+                    {...register("name")}
+                  />
+                  <InputController
+                    label="Correo electrónico"
+                    type="text"
+                    className="w-full lg:w-2/3"
+                    placeholder="Ingresa tu correo electrónico"
+                    error={errors.email?.message}
+                    {...register("email")}
+                  />
+                  <PhoneController
+                    label="Teléfono"
+                    error={errors.phone?.message}
+                    control={control}
+                    name="phone"
+                  />
+                </div>
+              </div>
+              <div className="rounded-lg border border-gray-200 bg-background p-3 py-6 md:gap-6">
+                <div className="flex flex-col gap-4">
+                  <h2 className="text-lg font-semibold">
+                    Información importante sobre el traslado
+                  </h2>
+                  <h3 className="text-sm">
+                    {searchParams?.booking?.vehicle?.name ?? "Traslado"}:{" "}
+                    {searchParams?.booking?.originZoneId?.name}-{" "}
+                    {parseDate(
+                      searchParams?.booking?.date,
+                      "yyyy-MM-dd",
+                      "EEE, d MMM"
+                    )}
+                    .
+                  </h3>
+                  <ul className="list-disc flex flex-col ml-2 pl-5 text-xs gap-2 text-gray-600">
+                    <li>
+                      Puedes cancelar sin costo hasta 24 horas antes del inicio
+                      de la actividad. Después de esa hora, no se hará ninguna
+                      cancelación, cambio o reembolso.
+                    </li>
+                    <li>
+                      Las actividades no se pueden transferir de una persona a
+                      otra.
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="rounded-lg border border-gray-200 bg-background p-3 py-6 md:gap-6">
+                <div className="flex flex-col gap-4">
+                  <h4 className="text-xs text-gray-600">
+                    Al hacer clic en el botón de abajo, acepto que revisé el
+                    Aviso de privacidad Se abrirá en una nueva ventana. y las
+                    Alertas de viaje del gobierno Se abrirá en una nueva
+                    ventana.. También acepto que revisé y estoy de acuerdo con
+                    las Normas y restricciones Se abrirá en una nueva ventana.,
+                    y los Términos de uso Se abrirá en una nueva ventana..
+                  </h4>
+                  <Button
+                    className="w-full lg:w-1/2"
+                    size="lg"
+                    onClick={onHandleClickBook}
+                    loading={isPending}
+                  >
+                    Completar reservación
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-span-1 lg:col-span-3">
+            <div className="flex flex-col gap-3">
+              <hr className="border-t border-gray-200" />
+              <ResumeDescription
+                vehicle={searchParams.booking.vehicle}
+                date={searchParams.booking.date}
+                hour={searchParams.booking.hour}
+                origin={searchParams.booking.originZoneId?.name}
+                destination={searchParams.booking.destinationZoneId?.name}
+              />
+
+              <hr className="border-t border-gray-200" />
+
+              <ResumePayment
+                price={searchParams.booking.vehicle.price}
+                paxes={searchParams.booking.paxes}
+                isShared={searchParams.booking.vehicle.isShared}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BookingPageContent;
---
+++ src/app/transfers/TransferPageContent.tsx
@@ -0,0 +1,162 @@
+"use client";
+import { BookingForm } from "@/components/BookingForm";
+import Pagination from "@/components/common/Pagination";
+import { CheckboxController } from "@/components/controllers/CheckboxController";
+import ComboBoxController from "@/components/controllers/SelectSimpleController";
+import TransferCard from "@/components/transfers/TransferCard";
+import { Spinner } from "@/components/ui/spinner";
+import { sortOptions } from "@/constant/sort";
+import useGetAvailablesTranfers from "@/hooks/query/useGetAvailablesTranfers";
+import { useDecodedSearchParams } from "@/hooks/useDecodedSearchParams";
+import useParseObjectToQueryUrl from "@/hooks/useParseObjectToQueryUrl";
+import { sumTotalPaxes } from "@/libs/utils";
+import { Pagination as PaginationType } from "@/types";
+import { QueryDTO, VehicleDTO } from "@/types/transfer";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { useForm, useWatch } from "react-hook-form";
+import z from "zod";
+
+const orderSchema = z
+  .object({
+    order: z.string(),
+    privateTransfer: z.boolean().optional(),
+    sharedTransfer: z.boolean().optional(),
+  })
+  .strict();
+
+export type OrderData = z.infer<typeof orderSchema>;
+
+const TransferPageContent: React.FC = () => {
+  const searchParams = useDecodedSearchParams();
+  const router = useRouter();
+  const [query, setQuery] = useState<QueryDTO>({
+    page: 1,
+    limit: 10,
+    originZoneId: searchParams?.booking?.originZoneId?.zone?._id || "",
+    destinationZoneId:
+      searchParams?.booking?.destinationZoneId?.zone?._id || "",
+    pax: sumTotalPaxes(searchParams?.booking?.paxes) || 1,
+  });
+  const queryPath = useParseObjectToQueryUrl(query);
+  const { data: items, isFetching } = useGetAvailablesTranfers(queryPath);
+  //console.log(items);
+  const { control } = useForm<OrderData>({
+    resolver: zodResolver(orderSchema),
+    defaultValues: {
+      order: "",
+      privateTransfer: false,
+      sharedTransfer: false,
+    },
+  });
+  const hasMounted = useRef(false);
+  const formValues = useWatch({ control });
+
+  useEffect(() => {
+    if (!hasMounted.current) {
+      hasMounted.current = true;
+      return;
+    }
+
+    let shared =
+      formValues.privateTransfer === formValues.sharedTransfer
+        ? undefined
+        : formValues.privateTransfer
+        ? false
+        : formValues.sharedTransfer;
+    setQuery({
+      page: 1,
+      limit: 10,
+      originZoneId: query.originZoneId,
+      destinationZoneId: query.destinationZoneId,
+      pax: query.pax,
+      shared,
+      sort: formValues.order === "" ? undefined : formValues.order,
+    });
+  }, [formValues]);
+
+  const handleClickCard = (vehicleSelected: VehicleDTO) => {
+    searchParams.booking.vehicle = vehicleSelected;
+    const params = new URLSearchParams({
+      booking: JSON.stringify(searchParams.booking),
+    });
+    router.push(`/book?${params.toString()}`);
+  };
+
+  const handleChangePagination = useCallback((pagination: PaginationType) => {
+    setQuery((prev) => ({
+      ...prev,
+      page: pagination.pageIndex + 1,
+      limit: pagination.pageSize,
+    }));
+  }, []);
+
+  if (isFetching)
+    return (
+      <div className="flex justify-center mt-12">
+        <Spinner className="text-gray-500" />
+      </div>
+    );
+
+  return (
+    <div className="relative h-screen">
+      <div className="mt-4 lg:mt-8 flex flex-col gap-14 justify-center max-w-5xl mx-auto pb-4 lg:pb-12 px-4 lg:px-0">
+        <BookingForm className="w-full" defaultInitial={searchParams.booking} />
+
+        <div className="grid grid-cols-1 lg:grid-cols-7">
+          <div className="col-span-1 lg:col-span-2">
+            <h2 className="mb-4 text-lg font-semibold">Filtrar por</h2>
+            <div className="flex flex-col [&_label]:font-light gap-2">
+              <h3 className="mb-2">Tipo de traslado</h3>
+              <CheckboxController
+                label="Traslado privado"
+                name="privateTransfer"
+                control={control}
+              />
+              <CheckboxController
+                label="Traslado compartido"
+                name="sharedTransfer"
+                control={control}
+              />
+            </div>
+          </div>
+          <div className="col-span-1 lg:col-span-5">
+            <div className="mb-3 flex justify-between items-end">
+              <h3 className="text-sm">
+                {items?.total ?? ""} resultados • El total incluye impuestos y
+                cargos
+              </h3>
+              <div>
+                <ComboBoxController
+                  name="order"
+                  label="Ordenar por"
+                  control={control}
+                  options={sortOptions}
+                  placeholder="Selecionar opción"
+                  isClearable
+                />
+              </div>
+            </div>
+            <div className="flex flex-col gap-3">
+              {items?.data.map((item: VehicleDTO) => (
+                <TransferCard
+                  key={item._id}
+                  handleClickCard={handleClickCard}
+                  vehicle={item}
+                />
+              ))}
+              <Pagination
+                handleChangePagination={handleChangePagination}
+                pageIndex={items?.page ?? 1}
+                pageSize={items?.pages ?? 1}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TransferPageContent;
